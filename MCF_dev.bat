8699bd987f30f5d46e53358cbc194c8dd92cd7e8
DragonettEx <rodanban251@gmail.com>
2023-06-25 16:13:52 +0300

    removed api key

--- MCF_dev.bat
+++ MCF_dev.bat
@@ -5,6 +5,6 @@ rem Активируем виртуальное окружение
 call .env\Scripts\activate.bat
 
 rem Запускаем Python скрипт
-py main.py RGAPI-5cfa8da7-3c6b-479c-ae1a-83d21019e6d7
+py main.py [riot-api-key]
 
 
