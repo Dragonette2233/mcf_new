53e0ac1bb2143c1f9996d90d5064457bdac86fa4
unknown <rodanban251@gmail.com>
2023-12-14 14:02:34 +0300

    fixes

--- mcf_automized_bot.py
+++ mcf_automized_bot.py
@@ -179,7 +179,7 @@ def open_stream_source():
 
     # time.sleep(1.5)
     pyautogui.click(x=1897, y=97)
-    time.sleep(4)
+    time.sleep(7)
     pyautogui.click(x=1871, y=354)
     time.sleep(1.5)
 
