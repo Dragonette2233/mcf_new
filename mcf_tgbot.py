9c34462d14e78947f040002222debb8bce664f5d
unknown <rodanban251@gmail.com>
2023-12-14 03:52:51 +0300

    g command added

--- mcf_tgbot.py
+++ mcf_tgbot.py
@@ -1,4 +1,5 @@
 from telegram import Update
+from telegram import ReplyKeyboardMarkup, KeyboardButton
 from telegram.ext import Application, CommandHandler, CallbackContext
 import os
 import logging
@@ -11,6 +12,13 @@ logging.getLogger("httpx").setLevel(logging.WARNING)
 
 logger = logging.getLogger(__name__)
 
+async def start(update: Update, context: CallbackContext):
+    keyboard = [ [KeyboardButton('/g')] ]
+    reply_markup = ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
+
+    await update.message.reply_text('Выберите вариант:', reply_markup=reply_markup)
+
+
 async def echo(update: Update, context: CallbackContext) -> None:
     """Echo the user message."""
     if update.message.text == '/g':
@@ -21,14 +29,13 @@ async def echo(update: Update, context: CallbackContext) -> None:
         except:
             await update.message.reply_text('Нет активной игры')
        
-
-
 def main() -> None:
     """Start the bot."""
     application = Application.builder().token(os.getenv('BOT_TOKEN')).build()
     g_handler = CommandHandler('g', echo)
+    application.add_handler(CommandHandler("start", start))
     application.add_handler(g_handler)
     application.run_polling(allowed_updates=Update.ALL_TYPES)
 
-# def start_polling():
-main()
+if __name__ == '__main__':
+    main()
